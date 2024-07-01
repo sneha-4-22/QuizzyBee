a8ff56a6d4a5538283109fb6756d802497b48b6f
sneha kumari <snehakumari64445@gmail.com>
2024-07-01 10:46:17 +0530

    change

--- backend/db.js
+++ backend/db.js
@@ -1,17 +1,18 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
-const mongoURI = 'mongodb+srv://admin:admin@quiz.2sgapwl.mongodb.net/quiz'
+const mongoURI = process.env.MONGO_URI;
 
 const connectToMongo = async () => {
-    try {
-      await mongoose.connect(mongoURI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-      console.log('Connected to MongoDB Successfully');
-    } catch (error) {
-      console.error('Failed to connect to MongoDB', error);
-    }
-  };
-  
-  module.exports = connectToMongo;
+  try {
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB Successfully');
+  } catch (error) {
+    console.error('Failed to connect to MongoDB', error);
+  }
+};
+
+module.exports = connectToMongo;
